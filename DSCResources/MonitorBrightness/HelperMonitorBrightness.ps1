490e26e5a0e9bd8ec652da75a4ecfe83f47b46f6
PRASOON KARUNAN V <kvprasoon@users.noreply.github.com>
2017-04-01 21:37:03 +0530

    Added files

---
+++ DSCResources/MonitorBrightness/HelperMonitorBrightness.ps1
@@ -0,0 +1,46 @@
+﻿Function Get-MonitorBrightness
+{
+[OutPutType([Int])]    
+    $WMIObject =Get-WmiObject -Class WMIMonitorBrightness -Namespace Root\WMI
+
+    Return $WMIObject.CurrentBrightness
+
+}
+
+Function Set-MonitorBrightness
+{
+
+Param(
+[Parameter(Mandatory)]
+[Int]$Level,
+
+$TimeOut = 5
+)
+    $WMIObject =Get-WmiObject -Class WMIMonitorBrightnessMethods -Namespace Root\WMI
+
+    $WMIObject.WmiSetBrightness($TimeOut,$Level)
+}
+
+Function Test-MonitorBritghness
+{
+[OutPutType([Bool])]
+Param(
+[Parameter(Mandatory)]
+[Int]$Level
+)
+    try{$WMIObject =Get-WmiObject -Class WMIMonitorBrightness -Namespace Root\WMI
+
+    if( $WMIObject.CurrentBrightness -eq $Level )
+    {
+        Return $true
+    }
+    else
+    {
+        Return $true
+    }
+    }
+    catch{
+        return $false
+    }
+}
+
---
+++ DSCResources/MonitorBrightness/Schema.ps1
@@ -0,0 +1,4 @@
+﻿$Property1 = New-xDscResourceProperty -Name Level -Type Sint16 -Attribute Key
+#$Property2 = New-xDscResourceProperty -Name TimeOut -Type Sint32 -Attribute Read
+
+New-xDscResource -Name MonitorBrightness -Property $Property1 -Path 'C:\Program Files\WindowsPowerShell\Modules\cMonitorBrightness' -FriendlyName MonitorBrightness -Force
