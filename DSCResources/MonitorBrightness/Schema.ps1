c90326ebbf730423a9b2f9f48f6dc08d36dd0c68
PRASOON KARUNAN V <kvprasoon@users.noreply.github.com>
2017-04-02 18:05:08 +0530

    Delete Schema.ps1

--- DSCResources/MonitorBrightness/Schema.ps1
+++
@@ -1,4 +0,0 @@
-﻿$Property1 = New-xDscResourceProperty -Name Level -Type Sint16 -Attribute Key
-#$Property2 = New-xDscResourceProperty -Name TimeOut -Type Sint32 -Attribute Read
-
-New-xDscResource -Name MonitorBrightness -Property $Property1 -Path 'C:\Program Files\WindowsPowerShell\Modules\cMonitorBrightness' -FriendlyName MonitorBrightness -Force
