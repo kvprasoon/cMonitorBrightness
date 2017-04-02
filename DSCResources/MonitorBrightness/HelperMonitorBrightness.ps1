6cabfb0ba14bb6529075873a8263a35037a0a026
PRASOON KARUNAN V <kvprasoon@users.noreply.github.com>
2017-04-02 18:08:07 +0530

    Add files via upload

---
+++ DSCResources/MonitorBrightness/HelperMonitorBrightness.ps1
@@ -0,0 +1,48 @@
+﻿Function Get-MonitorBrightness
+{
+[OutPutType([Int])]    
+    $WMIObject =Get-WmiObject -Class WMIMonitorBrightness -Namespace Root\WMI
+
+    Return $WMIObject.CurrentBrightness
+
+}
+
+Function Set-MonitorBrightness
+{
+
+Param(
+[Parameter(Mandatory)]
+[Int]$Level,
+
+$TimeOut = 5
+)
+    $WMIObject =Get-WmiObject -Class WMIMonitorBrightnessMethods -Namespace Root\WMI
+
+    $WMIObject.WmiSetBrightness($TimeOut,$Level)
+}
+
+Function Test-MonitorBritghness
+{
+[OutPutType([Bool])]
+Param(
+[Parameter(Mandatory)]
+[Int]$Level
+)
+    try{
+    
+    $WMIObject =Get-WmiObject -Class WMIMonitorBrightness -Namespace Root\WMI
+    
+    if( $WMIObject.CurrentBrightness -ne $Level )
+    {
+        Return $false
+    }
+    else
+    {
+        Return $true
+    }
+    }
+    catch{
+        return $false
+    }
+}
+
