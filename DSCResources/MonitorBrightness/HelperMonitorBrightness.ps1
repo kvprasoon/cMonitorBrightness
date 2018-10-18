f66c5630788807e11daf266a18025137bbc717b3
kvprasoon <prasoonkarunan5@hotmail.com>
2018-10-18 20:36:45 +0800

    Fomrating fixes

--- DSCResources/MonitorBrightness/HelperMonitorBrightness.ps1
+++ DSCResources/MonitorBrightness/HelperMonitorBrightness.ps1
@@ -1,48 +1,39 @@
-﻿Function Get-MonitorBrightness
-{
-[OutPutType([Int])]    
+﻿Function Get-MonitorBrightness{
+    [OutPutType([Int])]
     $WMIObject =Get-WmiObject -Class WMIMonitorBrightness -Namespace Root\WMI
-
     Return $WMIObject.CurrentBrightness
-
 }
 
-Function Set-MonitorBrightness
-{
+Function Set-MonitorBrightness{
+    Param(
+        [Parameter(Mandatory)]
+        [Int]$Level,
 
-Param(
-[Parameter(Mandatory)]
-[Int]$Level,
-
-$TimeOut = 5
-)
+        [Parameter(Mandatory=$false)]
+        [int]$TimeOut = 5
+    )
     $WMIObject =Get-WmiObject -Class WMIMonitorBrightnessMethods -Namespace Root\WMI
-
     $WMIObject.WmiSetBrightness($TimeOut,$Level)
 }
 
-Function Test-MonitorBritghness
-{
-[OutPutType([Bool])]
-Param(
-[Parameter(Mandatory)]
-[Int]$Level
-)
+Function Test-MonitorBritghness{
+    [OutPutType([Bool])]
+    Param(
+        [Parameter(Mandatory)]
+        [Int]$Level
+    )
     try{
-    
-    $WMIObject =Get-WmiObject -Class WMIMonitorBrightness -Namespace Root\WMI
-    
-    if( $WMIObject.CurrentBrightness -ne $Level )
-    {
-        Return $false
-    }
-    else
-    {
-        Return $true
-    }
+
+        $WMIObject =Get-WmiObject -Class WMIMonitorBrightness -Namespace Root\WMI
+
+        if( $WMIObject.CurrentBrightness -ne $Level ){
+            Return $false
+        }
+        else{
+            Return $true
+        }
     }
     catch{
         return $false
     }
 }
-
